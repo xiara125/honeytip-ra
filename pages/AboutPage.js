088b19fc2e496ab6a76f13c937ec52be042c2a25
xiara125 <xiara125@gmail.com>
2022-08-17 00:08:02 +0900

    test

--- pages/AboutPage.js
+++ pages/AboutPage.js
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { StatusBar } from 'expo-status-bar';
 import {StyleSheet, ScrollView, Text, View, Image, TouchableOpacity} from 'react-native'
+import * as Linking from 'expo-linking';
 
 const img = 'https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2FaboutImage.png?alt=media&token=13e1c4f6-b802-4975-9773-e305fc7475c4'
+const link = 'https://github.com/xiara125'
 
-export default function AboutPage(){
-  return (<ScrollView style={styles.container}>
-        <Text style={styles.title}>HI! 스파르타코딩 앱개발반에 오신것을 환영합니다</Text>
-        <View style={styles.mainContainer}>
-            <Image style={styles.mainImage}source={{url:img}}/>
-            <Text style={styles.text1}>많은 내용을 간결하게 담아내려 노력했습니다!</Text>
-            <Text style={styles.text2}>꼭 완주 하셔서 꼭 여러분것으로 만들어가시길 바랍니다</Text>
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.text3}>여러분의 인스타 계정</Text>
-            </TouchableOpacity>
-        </View>
-      </ScrollView>)
+export default function AboutPage({navigation, route}){
+    useEffect(()=>{
+        navigation.setOptions({
+            title: "소개페이지",
+            headerStyle: {
+                backgroundColor: "darkblue",
+                shadowColor: "darkblue",
+            },
+            headerTintColor: "white",
+        })
+    },[])
+  
+    return (
+        <ScrollView style={styles.container}>
+            <StatusBar style="light" />
+            <Text style={styles.title}>HI! 스파르타코딩 앱개발반에 오신것을 환영합니다</Text>
+            <View style={styles.mainContainer}>
+                <Image style={styles.mainImage}source={{url:img}}/>
+                <Text style={styles.text1}>많은 내용을 간결하게 담아내려 노력했습니다!</Text>
+                <Text style={styles.text2}>꼭 완주 하셔서 꼭 여러분것으로 만들어가시길 바랍니다</Text>
+                <TouchableOpacity style={styles.button} onPress={()=>Linking.openURL(link)}>
+                    <Text style={styles.text3}>여러분의 인스타 계정</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>)
 }
 
 const styles = StyleSheet.create({
+    detail: {
+        flex:1,
+        flexDirection:"row",
+        margin:10, 
+        borderBottomWidth:0.5,
+        borderBottomColor:"#eee",
+        paddingBottom:10
+    },
     container: {
         backgroundColor : 'darkblue',
         padding: 35,
